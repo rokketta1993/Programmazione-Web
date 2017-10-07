41e39bdef73b5b624788f0a57db2d76253480649
MATTIA <rokketta1993>
2017-10-07 16:19:06 +0200

    deployed-heroku

--- routes/users.js
+++ routes/users.js
@@ -158,6 +158,7 @@ router.post('/login', passport.authenticate('local',
 router.get('/logout', function(req, res){
 	req.logout();
 
+    req.flash('success_msg', 'Logout effettuato con successo');
 	res.redirect('/users/login');
 });
 
